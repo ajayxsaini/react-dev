be23590527dcff151d18b5dc77259a595ef288d8
ajayxsaini <153013883+ajayxsaini@users.noreply.github.com>
2025-01-08 10:41:18 +0000

    here it is

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+      <h1>
+        Hello world | this is Ajay, gotta learn some more.
+      </h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <h1>
+      Hey there | Again me only
+    </h1>
+      )
+}
+
+export default App
